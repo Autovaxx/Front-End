1b940e31ffc7e95e455645236f717d90fc4c5378
christya95 <christya@sheridancollege.ca>
2022-10-25 00:23:51 -0400

    Search Preferences dropdown, and dates added.

--- components/datePicker.js
+++ components/datePicker.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import {
-  Modal,
   StyleSheet,
   Text,
-  TouchableHighlight,
+  TouchableOpacity,
   View,
   Platform,
 } from "react-native";
@@ -12,34 +11,96 @@ import moment from "moment";
 
 const CustomDatePicker = (props) => {
   const [date, setDate] = useState(moment());
-  // const [show, setShow] = useState(false);
+  const [display, setDisplay] = useState(false);
 
-  const onChange = (e, selectedDate) => {
+  const onIOSChange = (e, selectedDate) => {
+    setDisplay(false);
     setDate(moment(selectedDate));
   };
 
+  const onAndroidChange = (e, selectedDate) => {
+    setDisplay(false);
+    setDate(moment(selectedDate));
+    props.onDateChange(selectedDate);
+  };
+
+  const renderAndroidDatePicker = () => {
+    return (
+      <View>
+        {display ? (
+          <DateTimePicker
+            timeZoneOffsetInMinutes={0}
+            value={new Date(date)}
+            mode="date"
+            minimumDate={new Date(moment().format("YYYY-MM-DD"))}
+            maximumDate={
+              new Date(moment().add(10, "years").format("YYYY-MM-DD"))
+            }
+            onChange={onAndroidChange}
+          />
+        ) : null}
+      </View>
+    );
+  };
+
+  const renderIOSDatePicker = () => {
+    return (
+      <View>
+        <DateTimePicker
+          timeZoneOffsetInMinutes={0}
+          value={new Date(date)}
+          mode="date"
+          minimumDate={new Date(moment().format("YYYY-MM-DD"))}
+          maximumDate={new Date(moment().add(10, "years").format("YYYY-MM-DD"))}
+          onChange={onIOSChange}
+        />
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <DateTimePicker
-        timeZoneOffsetInMinutes={0}
-        value={new Date(date)}
-        mode="date"
-        minimumDate={new Date(moment().format("YYYY-MM-DD"))}
-        maximumDate={new Date(moment().add(10, "years").format("YYYY-MM-DD"))}
-        onChange={onChange}
-      />
+    <View
+      style={[
+        styles.container,
+        {
+          paddingTop: Platform.OS === "ios" ? "3%" : "2%",
+        },
+      ]}
+    >
+      {Platform.OS === "ios" ? null : (
+        <TouchableOpacity
+          style={styles.androidDate}
+          onPress={() => setDisplay(true)}
+        >
+          <Text style={styles.androidDateText}>
+            {moment(date).format("MMM DD, YYYY")}
+          </Text>
+        </TouchableOpacity>
+      )}
+      {Platform.OS === "ios"
+        ? renderIOSDatePicker()
+        : renderAndroidDatePicker()}
     </View>
   );
 };
 
-CustomDatePicker.defaultProps = {
-  // textStyle: {},
-};
+CustomDatePicker.defaultProps = {};
 export default CustomDatePicker;
 
 const styles = StyleSheet.create({
   container: {
     width: "50%",
-    paddingTop: "3%",
+    padding: "5%",
+    fontSize: 12,
+  },
+  androidDate: {
+    width: "90%",
+    backgroundColor: "#dddddd",
+    borderColor: "red",
+    padding: "2%",
+    borderRadius: 8,
+  },
+  androidDateText: {
+    fontSize: 20,
   },
 });
--- screens/SearchPrefScreen.js
+++ screens/SearchPrefScreen.js
@@ -4,41 +4,118 @@ import {
   Text,
   TouchableOpacity,
   View,
+  ScrollView,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import Constants from "expo-constants";
 import { useNavigation } from "@react-navigation/native";
 import CustomDatePicker from "../components/datePicker";
+import SelectList from "react-native-dropdown-select-list";
 
 const SearchPrefScreen = () => {
   const navigation = useNavigation();
+  const pharmacies = ["Shoppers", "Rexall", "Metro"];
+  const vaccinationPrefs = [
+    "Astrazeneca",
+    "Pfizer",
+    "Moderna",
+    "Johnson & Johnson",
+  ];
+
+  const [pharmacy, setPharmacy] = useState("");
+  const [vaccinationPref, setVaccinationPref] = useState("");
+
   const handleHome = () => {
     console.log("View Home page");
     navigation.navigate("Home");
   };
   const StatusBarHeight = Constants.StatusBarHeight;
   return (
-    <KeyboardAvoidingView
-      style={[styles.container, { marginTop: StatusBarHeight }]}
-      behavior="padding"
-    >
-      <View style={styles.innerContainer}>
-        <View style={styles.innerTitleContainer}>
-          <TouchableOpacity onPress={handleHome}>
-            <Ionicons name="arrow-back" size={25} />
-          </TouchableOpacity>
-          <Text style={styles.title}>Applications</Text>
-          <TouchableOpacity>
-            <Ionicons name="notifications" size={25} />
+    <ScrollView>
+      <KeyboardAvoidingView
+        style={[styles.container, { marginTop: StatusBarHeight }]}
+        behavior="padding"
+      >
+        <View style={styles.innerContainer}>
+          <View style={styles.innerTitleContainer}>
+            <TouchableOpacity onPress={handleHome}>
+              <Ionicons name="arrow-back" size={25} />
+            </TouchableOpacity>
+            <Text style={styles.title}>Applications</Text>
+            <TouchableOpacity>
+              <Ionicons name="notifications" size={25} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.innerDateContainer}>
+          <Text style={styles.datetitle}>Start Date:</Text>
+          <CustomDatePicker />
+        </View>
+        <View style={styles.innerDateContainer}>
+          <Text style={styles.datetitle}>End Date:</Text>
+          <CustomDatePicker />
+        </View>
+        <View style={styles.innerDateContainer}>
+          <Text style={styles.subtitle}>Pharmacy</Text>
+        </View>
+        <View>
+          <Text style={styles.subtitleText}>
+            Select the pharmacies you'd like to make a booking at.
+          </Text>
+        </View>
+        <View>
+          <SelectList
+            setSelected={setPharmacy}
+            data={pharmacies}
+            placeholder="Pharmacy"
+            search={false}
+            arrowicon={<Ionicons name="arrow-down" size={24} color="#2699FB" />}
+            inputStyles={{ marginBottom: "1.5%" }}
+            boxStyles={{
+              borderColor: "#FB2876",
+              borderWidth: 1,
+              borderRadius: 8,
+              marginBottom: "1.5%",
+            }}
+          />
+        </View>
+        <View style={styles.innerDateContainer}>
+          <Text style={styles.subtitle}>Vaccinaiton Preference</Text>
+        </View>
+        <View>
+          <Text style={styles.subtitleText}>
+            Select the type of vaccine you wish to recieve.
+          </Text>
+        </View>
+        <View>
+          <SelectList
+            setSelected={setVaccinationPref}
+            data={vaccinationPrefs}
+            placeholder="Vaccination Preferences"
+            search={false}
+            arrowicon={<Ionicons name="arrow-down" size={24} color="#2699FB" />}
+            inputStyles={{ marginBottom: "1.5%" }}
+            boxStyles={{
+              borderColor: "#FB2876",
+              borderWidth: 1,
+              borderRadius: 8,
+              marginBottom: "1.5%",
+            }}
+          />
+        </View>
+        <View
+          style={[
+            styles.innerContainer,
+            { width: "50%", alignSelf: "center", marginTop: "15%" },
+          ]}
+        >
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Save</Text>
           </TouchableOpacity>
         </View>
-      </View>
-      <View style={styles.innerDateContainer}>
-        <Text style={styles.datetitle}>Start Date:</Text>
-        <CustomDatePicker />
-      </View>
-    </KeyboardAvoidingView>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 
@@ -53,7 +130,7 @@ const styles = StyleSheet.create({
   innerContainer: {
     flexDirection: "row",
     width: "100%",
-    justifyContent: "center",
+    justifyContent: "space-between",
   },
   innerTitleContainer: {
     width: "100%",
@@ -82,5 +159,26 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     padding: "3%",
+  },
+  subtitleText: {
+    padding: "3%",
+  },
+  buttonContainer: {
+    width: "80%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#FB2876",
+    width: "100%",
+    padding: 12,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
   },
 });
